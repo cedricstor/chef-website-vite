1fae23a426415fed500f0535f74091ccb1a1fdef
cedricstor <cedricbendavid@gmail.com>
2025-07-24 01:06:39 -0400

    src

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Menu from './pages/Menu.jsx'
+import Contact from './pages/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 )
---
+++ src/pages/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1>About Chef Ced</h1>
+}
